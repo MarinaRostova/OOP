255cd0087a68cce39612454c1b2da1c41a515b56
Marina Lobtsova <dvl01@yandex.ru>
2024-05-29 22:22:50 +0300

    add file4

--- OOPhomework.py
+++ OOPhomework.py
@@ -6,11 +6,9 @@ class Student:
         self.finished_courses = []
         self.courses_in_progress = []
         self.grades = {}
-        self.grades_student = grades_student
-        student1 = Student("Ruoy", "Eman")
-        student2 = Student()
-    f'Имя: {self.name} \\nФамилия: {self.surname} \\nСредняя оценка за домашние задания: {self.rate_avg()} \\nКурсы в процессе изучения: {" | ".join(self.courses_in_progress)} \\nЗавершенные курсы: {" | ".join(self.finished\_courses)}'
-    1234567891011
+
+    def rate_hw(self):
+        return self.rate_hw
 
     def __lt__(self, other):
         if not isinstance(other, Student):
@@ -24,95 +22,52 @@ class Student:
                 return f'Студенты: {other.name} {other.surname} и {self.name} {self.surname} равнозначны по успеваемости'
 
 
-    student_1 = Student('Ruoy', 'Eman', 'female')
-    best_student.courses_in_progress += ['Python']
-    best_student.courses_in_progress += ['Git']
-    best_student.finished_courses += ['Введение в программирование']
-    print(student_1)
+    def _str_(self):
+        return f'Имя: {self.name} \nФамилия: {self.surname} \nСредняя оценка за домашние задания: {self.rate_hw()} \nКурсы в процессе изучения: {" | ".join(self.courses_in_progress)} \nЗавершенные курсы: {" | ".join(self.finished_courses)}'
 
-    def rate_hw(self, lecturer, course, grade):
-        if isinstance(lecturer, Lecturer) and course in self.courses_attached and course in lecturer.courses_in_progress:
-            if course in lecturer.grades:
-                lecturer.grades[course] += [grade]
-            else:
-                lecturer.grades[course] = [grade]
-        else:
-            return 'Ошибка'
-
-best_lecturer = Lecturer('Rona', 'Emana', 'your_gender')
-best_lecturer.courses_in_progress += ['Python']
-
-print(student1 <= student2)
-print(student1 > student2)
-print(student1 == student2)
-print(student1)
+    some_student = Student('Ruoy', 'Eman', 'female')
+    some_student.courses_in_progress += ['Python']
+    some_student.courses_in_progress += ['Git']
+    some_student.finished_courses += ['Введение в программирование']
 
 
-
-
-
-
-
-cool_student = Student('Somet', 'Buddyes')
-cool_student.courses_attached += ['Python']
-
-cool_student.rate_hw(best_lecturer, 'Python', 10)
-cool_student.rate_hw(best_lecturer, 'Python', 10)
-cool_student.rate_hw(best_lecturer, 'Python', 10)
-
-print(best_lecturer.grades)
-
-
-
+    print(some_student)
 
 
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
+        self.grades = {}
+        self.courses = []
         self.courses_attached = []
-
-
 
 class Lecturer (Mentor):
-    def __init__(self, name, surname):
-        self.name = name
-        self.surname = surname
-        self.courses_attached = []
-        self.grades = {}
-        self.courses =[]
-        self.grades_lecturer = grades_lecturer
-
-    def __str__(self, name, surname, av_grades_lecturer):
-        return f'{self, name, surname, av_grades_lecturer}'
 
 
-sum_lecturer = 0
-sum_grades = 0
-count_lecture = 0
+    def rate_avg(self):
+        return self.rate_avg
+    def __str__(self):
+        return f'Имя: {self.name} \nФамилия: {self.surname} \nСредняя оценка за лекции: {self.rate_avg()}'
+    def av_grade_lecture(self, course, *students):
+        if not isinstance(course, str):
+            return "Ошибка"
+    sum_lecturer = 0
+    count_lecture = 0
+
+    for student in students:
+          sum_lecture += sum(self.grades(course, []))/ len(self.grades(course, []))
+          count_lecture += 1
+          result = round(sum_lecture / count_lecture, 1)
+                return result
+
+
 
 some_lecturer = Lecturer('Some', 'Buddy', '9.9')
-some1_lecturer = Lecturer()
-    for student in students:
-          sum_lecture += sum(self.grades(course, []) / len(self.grades(course, [])
-          count_lecture += 1
-      result = av_grades_lecturer = round(sum_lecture / count_lecture, 1)
-      return result
+some_lecturer.rate_avg(best_lecturer, 'Python', 10)
+some_lecturer.rate_avg(best_lecturer, 'Python', 10)
+some_lecturer.rate_avg(best_lecturer, 'Python', 10)
 
-
-def _le_ (self, grades_lecturer):
-        return some_lecturer <= some1_lecturer
-
-def _gt_(self, grades_lecturer):
-    return some_lecturer > some1_lecturer
-
-def _eg_(self, grades_lecturer):
-    return some_lecturer == some1_lecturer
-
-
-print(some_lecturer <= some1_lecturer)
-print(some_lecturer > some1_lecturer)
-print(some_lecturer == some1_lecturer)
 print(some_lecturer)
 
 
@@ -120,12 +75,9 @@ print(some_lecturer)
 
 
 class Reviewer (Mentor):
-    def __init__(self, name, surname):
-        self.name = name
-        self.surname = surname
-        self.courses_attached = []
-    def __str__(self, name, surname):
-        return f'{self, name, surname}'
+
+    def __str__(self):
+        return f'Имя: {self.name} \\nФамилия: {self.surname}'
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
             if course in student.grades:
@@ -134,20 +86,18 @@ class Reviewer (Mentor):
                 student.grades[course] = [grade]
         else:
             return 'Ошибка'
-    def str (self):
-        return (f'Имя: {self.name} Фамилия: {self.surname}')
-best_student = Student('Ruoy', 'Eman', 'your_gender')
-best_student.courses_in_progress += ['Python']
+
+
 
 some_reviewer = Reviewer('Some', 'Buddy')
-cool_reviewer = Reviewer('Somer', 'Buddys')
-cool_reviewer.courses_attached += ['Python']
 
-cool_reviewer.rate_hw(best_student, 'Python', 10)
-cool_reviewer.rate_hw(best_student, 'Python', 10)
-cool_reviewer.rate_hw(best_student, 'Python', 10)
+some_reviewer.courses_attached += ['Python']
 
-print(best_student.grades)
+
+some_reviewer.rate_hw(best_student, 'Python', 10)
+some_reviewer.rate_hw(best_student, 'Python', 10)
+some_reviewer.rate_hw(best_student, 'Python', 10)
+
 print(some_reviewer)
 
 
